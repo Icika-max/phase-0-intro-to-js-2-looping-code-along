1d8993adbbc2ad0187ef73101be093507327c5e2
icika-max <icika-max>
2022-11-19 14:51:46 +0300

    completed assignmnt

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const event=["Ada", "Brendan", "Ali"];
+
+function whiteCards(event) {
+    for (let i =0; i < whiteCards.length; i++){
+        console.log('Thank you ${event[i]} for the wonderful birthday gift!');
+        debugger;
+    }
+    return event;
+}
+
+whiteCards('event');
+
+let countdown = 10;
+while (countdown >= 0) {
+  console.log(countup++);
+}
