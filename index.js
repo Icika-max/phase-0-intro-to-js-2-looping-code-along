9543428228a7e75474aed5b035be4f35596335a9
icika-max <icika-max>
2022-12-13 22:11:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-// Code your solutions in this file
-const event=["Ada", "Brendan", "Ali"];
-
-function whiteCards(event) {
-    for (let i =0; i < whiteCards.length; i++){
-        console.log('Thank you ${event[i]} for the wonderful birthday gift!');
-        debugger;
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   }
+// let string_name = ["Ada", "Brendan", "Ali"];
+// let event_name = "birthday";
+// const string_name = [];
+// let event_name = "";
+function writeCards(string_name, event_name)
+{
+    var array_store = []; 
+    for (let counter = 0; counter < string_name.length; counter++)
+    {    
+        array_store.push(`Thank you, ${string_name[counter]}, for the wonderful ${event_name} gift!`);
+        
     }
-    return event;
+    return array_store;
 }
 
-whiteCards('event');
-
-let countdown = 10;
-while (countdown >= 0) {
-  console.log(countup++);
-}
+ function countDown(integer)
+ {
+    
+    while (integer >= 0)
+    {
+        console.log(integer)
+        integer--;
+    }
+ }
